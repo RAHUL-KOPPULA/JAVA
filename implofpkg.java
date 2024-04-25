be92bc3738e3dd3a9f7cab5b46cd73807796c01f
RAHUL BABU_KOPPULA <160829817+RAHUL-KOPPULA@users.noreply.github.com>
2024-04-25 12:57:17 +0530

    Add files via upload

---
+++ implofpkg.java
@@ -0,0 +1,11 @@
+import arithmeticop.*;
+class addsubimp
+{
+ public static void main(String[] args)
+{
+ addition a1=new addition(4,5);
+ a1.sum();
+ subtraction s1=new subtraction(5,6);
+ s1.difference();
+}
+}
